44b0934f0a5df43fde67733b6fc2504d6604a103
jck <jckleinbourg@gmail.com>
2024-07-04 23:01:23 +0200

    refactor: make web folder

--- web/scripts/postinstall.js
+++ web/scripts/postinstall.js
@@ -5,5 +5,5 @@ const ablyPath = path.join(__dirname, "../node_modules/ably/build/ably.min.js");
 const jqueryPath = path.join(__dirname, "../node_modules/jquery/dist/jquery.min.js");
 
 
-fs.copyFileSync(ablyPath, path.join(__dirname, "../web/app/js/libs/ably/ably.min.js"));
-fs.copyFileSync(jqueryPath, path.join(__dirname, "../web/app/js/libs/jquery/jquery.min.js"));
+fs.copyFileSync(ablyPath, path.join(__dirname, "../app/js/libs/ably/ably.min.js"));
+fs.copyFileSync(jqueryPath, path.join(__dirname, "../app/js/libs/jquery/jquery.min.js"));
